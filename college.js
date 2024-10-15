a4c2babdc8de5144c10f3885d7db221f76aa2848
Kondam Pravalika Reddy <kpravalika426@gmail.com>
2024-10-15 17:11:07 +0530

    first commit

---
+++ college.js
@@ -0,0 +1,16 @@
+let collegeName="CDC Deemed to University";
+console.log(collegeName);
+let collegeN=new String("CDC college");
+console.log(collegeN);
+console.log(collegeName.length);
+console.log(collegeName.toLowerCase());
+console.log(collegeName.toUpperCase());
+console.log(collegeName.charAt('2'));
+console.log(collegeName.startsWith ('cdc'));
+console.log(collegeName.startsWith ('CDC'));
+console.log(collegeName.endsWith ('university'));
+console.log(collegeName.endsWith ('University'));
+console.log(collegeName.includes ('Deemed'));
+console.log(collegeName.includes ('DEEMED'));
+console.log(collegeName.repeat ('3'));
+console.log(`hii ${collegeName}`);
